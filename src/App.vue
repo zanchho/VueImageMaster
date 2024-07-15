74c0a119dd4bceee420da9f610e0f2efaa4630b4
Philipp <74034252+zanchho@users.noreply.github.com>
2024-07-15 07:54:12 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p>Welcome to my Vite light weight startup.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+}
+</script>
